57cbf601c03abcf58d9b805817385bc5d5bc2ac7
rinse <rinse418@gmail.com>
2023-12-26 03:28:48 +0900

    feat: Enable Katex

--- build.bash
+++ build.bash
@@ -5,7 +5,8 @@ OUTPUT_DIR=./build
 rm -fr "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
-pandoc index.md -t revealjs -s -o build/index.html --slide-level 3 \
-    -V revealjs-url=https://unpkg.com/reveal.js@4.5.0
+pandoc index.md -t revealjs -s -o "$OUTPUT_DIR/index.html" --slide-level 3 \
+    -V revealjs-url=https://unpkg.com/reveal.js@4.5.0 \
+    --katex
 
 cp -r images "$OUTPUT_DIR/"
