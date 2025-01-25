d948b104ddfdc38cb60cf32cc4c2870285287191
rinse <rinse418@gmail.com>
2025-01-25 19:50:45 +0900

    improve the build process

--- build.bash
+++ build.bash
@@ -5,6 +5,9 @@ OUTPUT_DIR=./out
 rm -fr "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
+cp -r images "$OUTPUT_DIR/"
+cp index.css include-in-header.html include-before-body.html include-after-body.html "$OUTPUT_DIR/"
+
 # For further options:
 # https://pandoc.org/chunkedhtml-demo/6.2-variables.html#variables-for-html-slides
 
@@ -14,7 +17,7 @@ mkdir -p "$OUTPUT_DIR"
 pandoc index.md -t revealjs -s -o "$OUTPUT_DIR/index.html" --slide-level 3 \
     -V revealjs-url=https://unpkg.com/reveal.js@5.1.0 \
     -V slideNumber=true \
-    --include-in-header=<(cat ./custom.css | sed -e '1i<style>' -e '$a</style>') \
+    --include-in-header=include-in-header.html \
+    --include-before-body=include-before-body.html \
+    --include-after-body=include-after-body.html \
     --katex
-
-cp -r images "$OUTPUT_DIR/"
